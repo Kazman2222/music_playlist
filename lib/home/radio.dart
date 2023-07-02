c40b0305531e8a993630c13e53a6ba82a7314129
Niklaus <iyioluwa@iyi's.hp.local>
2023-07-02 19:08:35 +0100

    Now Functioning..

--- lib/home/radio.dart
+++ lib/home/radio.dart
@@ -8,115 +8,72 @@ import '../bottomnavigationbar/bottomnavigationbar.dart';
 import '../constants.dart';
 
 class radio extends StatefulWidget {
-  const radio({super.key});
+  const radio({Key? key}) : super(key: key);
 
   @override
-  State<radio> createState() => _radioState();
+  State<radio> createState() => _RadioPageState();
 }
 
-// Random random = Random();
-//
-// // Generate a random integer within a range
-// int randomNumber =
-//     random.nextInt(5); // Generates a random number between 0 and 99
-//
-// // print(randomNumber) {
-// //   // TODO: implement print
-// //   throw UnimplementedError();
-// // }
-
-class _radioState extends State<radio> {
-  // String image_1 = 'assets/pics/radio_hz1.png';
-  // String image_2 = 'assets/pics/radio_hz2.png';
-  // String image_3 = 'assets/pics/radio_hz3.png';
-  // String image_4 = 'assets/pics/radio_hz4.png';
-
-  // List<String> images = [
-  //   'assets/pics/radio_hz1.png',
-  //   'assets/pics/radio_hz2.png',
-  //   'assets/pics/radio_hz3.png',
-  //   'assets/pics/radio_hz3.png'
-  // ];
-
-  // Random random = Random();
+class _RadioPageState extends State<radio> {
+  // bool tapper = false;
   //
-  // // Generate a random integer within a range
-  // int randomNumber = random.nextInt(5); // Generates a random number between 0 and 99
-  //
-  // print(randomNumber);
-
-  // print();
-
-  // int colorPicker() {
-  //   Random random = Random();
-  //
-  //   int randomNumber = random.nextInt(5); // Generate a random whole number between 0 and 99
-  //
-  //   return randomNumber;
+  // void toggleContainerVisibility() {
+  //   setState(() {
+  //     tapper = !tapper;
+  //   });
   // }
 
   @override
   Widget build(BuildContext context) {
-    bool tapper = false;
-
-    bool toggleContainerVisibility() {
-      setState(() {
-        tapper = !tapper;
-      });
-      return tapper;
-    }
-
     return Scaffold(
       backgroundColor: kBackGroundColour,
       body: SafeArea(
         top: true,
         child: SingleChildScrollView(
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
                   height: 10.h,
                 ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.h),
-                  child: Row(
-                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const profilePage()),
-                              );
-                            },
-                            child: CircleAvatar(
-                              backgroundColor:
-                                  const Color.fromARGB(255, 71, 224, 76),
-                              radius: 20.r,
-                              child: const Text('J',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20,
-                                      color: Colors.black)),
-                            ),
+                Row(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const profilePage(),
                           ),
-                          SizedBox(
-                            width: 10.w,
+                        );
+                      },
+                      child: CircleAvatar(
+                        backgroundColor: const Color.fromARGB(255, 71, 224, 76),
+                        radius: 20.r,
+                        child: const Text(
+                          'J',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.black,
                           ),
-                          const Text('Radio',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20))
-                        ],
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      width: 10.w,
+                    ),
+                    const Text(
+                      'Radio',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(
                   height: 20.h,
@@ -126,10 +83,12 @@ class _radioState extends State<radio> {
                   children: [
                     TextButton(
                       style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          primary: Colors.black),
-                      onPressed: (() {}),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        primary: Colors.black,
+                      ),
+                      onPressed: () {},
                       child: Padding(
                         padding: const EdgeInsets.all(5),
                         child: Text(
@@ -144,10 +103,12 @@ class _radioState extends State<radio> {
                     ),
                     TextButton(
                       style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          primary: Colors.green),
-                      onPressed: (() {}),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        primary: Colors.green,
+                      ),
+                      onPressed: () {},
                       child: Padding(
                         padding: const EdgeInsets.all(5),
                         child: Text(
@@ -162,10 +123,12 @@ class _radioState extends State<radio> {
                     ),
                     TextButton(
                       style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          primary: Colors.black),
-                      onPressed: (() {}),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        primary: Colors.black,
+                      ),
+                      onPressed: () {},
                       child: Padding(
                         padding: const EdgeInsets.all(5),
                         child: Text(
@@ -189,12 +152,14 @@ class _radioState extends State<radio> {
                     RadioSelector(
                       radioChanel: 'Jazz FM',
                       radioStation: '106.5',
-                      checker: toggleContainerVisibility,
+                      // checker: tapper,
+                      // onTap: toggleContainerVisibility,
                     ),
                     RadioSelector(
                       radioChanel: 'RayMax FM',
                       radioStation: '91.3',
-                      checker: toggleContainerVisibility,
+                      // checker: tapper,
+                      // onTap: toggleContainerVisibility,
                     ),
                   ],
                 ),
@@ -207,12 +172,54 @@ class _radioState extends State<radio> {
                     RadioSelector(
                       radioChanel: 'Cool FM',
                       radioStation: '99.6',
-                      checker: toggleContainerVisibility,
+                      // checker: tapper,
+                      // onTap: toggleContainerVisibility,
                     ),
                     RadioSelector(
                       radioChanel: 'Max FM',
                       radioStation: '103.5',
-                      checker: toggleContainerVisibility,
+                      // checker: tapper,
+                      // onTap: toggleContainerVisibility,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RadioSelector(
+                      radioChanel: 'Jazz FM',
+                      radioStation: '106.5',
+                      // checker: tapper,
+                      // onTap: toggleContainerVisibility,
+                    ),
+                    RadioSelector(
+                      radioChanel: 'RayMax FM',
+                      radioStation: '91.3',
+                      // checker: tapper,
+                      // onTap: toggleContainerVisibility,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.h,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RadioSelector(
+                      radioChanel: 'Cool FM',
+                      radioStation: '99.6',
+                      // checker: tapper,
+                      // onTap: toggleContainerVisibility,
+                    ),
+                    RadioSelector(
+                      radioChanel: 'Max FM',
+                      radioStation: '103.5',
+                      // checker: tapper,
+                      // onTap: toggleContainerVisibility,
                     ),
                   ],
                 ),
@@ -226,28 +233,26 @@ class _radioState extends State<radio> {
   }
 }
 
-class RadioSelector extends StatelessWidget {
-  // const RadioSelector({
-  //   Key? key,
-  // }) : super(key: key);
+class RadioSelector extends StatefulWidget {
+  RadioSelector({
+    required this.radioStation,
+    required this.radioChanel,
+  });
 
+  final String radioStation;
+  final String radioChanel;
+
+  @override
+  State<RadioSelector> createState() => _RadioSelectorState();
+}
+
+class _RadioSelectorState extends State<RadioSelector> {
   int colorPicker() {
     Random random = Random();
-
     int randomNumber =
-        random.nextInt(4); // Generate a random whole number between 0 and 99
-
+        random.nextInt(4); // Generate a random number between 0 and 3
     return randomNumber;
   }
-
-  RadioSelector(
-      {required this.radioStation,
-      required this.radioChanel,
-      required this.checker});
-
-  late String radioStation;
-  late String radioChanel;
-  late bool Function() checker;
 
   List<String> images = [
     'assets/pics/radio_hz1.png',
@@ -256,18 +261,33 @@ class RadioSelector extends StatelessWidget {
     'assets/pics/radio_hz4.png'
   ];
 
-  // bool condition = checker as bool;
+  bool tapper = false;
+  bool hearts = false;
+
+  void toggleContainerVisibility() {
+    setState(() {
+      tapper = !tapper;
+    });
+  }
+
+  void toggleHearts() {
+    setState(() {
+      hearts = !hearts;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: checker,
+      onTap: toggleContainerVisibility, // Call the provided onTap callback
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
           border: Border.all(
-              width: 3.0,
-              style: BorderStyle.solid,
-              color: checker ? Colors.green : Colors.white),
+            width: 3.0,
+            style: BorderStyle.solid,
+            color: tapper ? Colors.green : Colors.white,
+          ),
         ),
         height: 250,
         width: 170,
@@ -283,7 +303,7 @@ class RadioSelector extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        radioStation,
+                        widget.radioStation,
                         style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 23,
@@ -294,18 +314,19 @@ class RadioSelector extends StatelessWidget {
                         height: 10,
                       ),
                       Text(
-                        radioChanel,
+                        widget.radioChanel,
                         style: const TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 18,
-                            color: Colors.white),
-                      )
+                          fontWeight: FontWeight.w400,
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
                     ],
                   ),
                   GestureDetector(
-                    onTap: () {},
-                    child: const Icon(
-                      Icons.favorite_outline,
+                    onTap: toggleHearts,
+                    child: Icon(
+                      hearts ? Icons.favorite : Icons.favorite_outline_sharp,
                       color: Colors.green,
                       size: 35,
                     ),
@@ -316,7 +337,6 @@ class RadioSelector extends StatelessWidget {
                 images[colorPicker()],
                 height: 70,
                 width: double.infinity,
-                // alignment: Alignment.bottomCenter,
               ),
             ],
           ),
@@ -325,45 +345,3 @@ class RadioSelector extends StatelessWidget {
     );
   }
 }
-
-// Slider(
-// activeColor: Colors.white,
-// min: 0.0,
-// max: 100.0,
-// value: 20,
-// onChanged: ((value) {})),
-// Row(
-// mainAxisAlignment: MainAxisAlignment.spaceBetween,
-// children: const [
-// Text(
-// '15:25',
-// style: TextStyle(
-// color: Colors.white,
-// fontWeight: FontWeight.bold),
-// ),
-// Text('27:30',
-// style: TextStyle(
-// color: Colors.white,
-// fontWeight: FontWeight.bold))
-// ],
-// ),
-// const SizedBox(
-// height: 10,
-// ),
-// Row(
-// mainAxisAlignment: MainAxisAlignment.spaceBetween,
-// children: const [
-// Icon(Icons.loop_outlined,
-// color: Color.fromARGB(255, 244, 242, 242),
-// size: 28),
-// Icon(Icons.skip_previous_outlined,
-// color: Colors.white, size: 28),
-// Icon(
-// Icons.play_circle,
-// color: Colors.white,
-// size: 28,
-// ),
-// Icon(Icons.skip_next_outlined,
-// color: Colors.white, size: 28),
-// Icon(Icons.volume_up_outlined,
-// color: Colors.white, size: 28),
